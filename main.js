e4ae338b3a8c8d4e222341e5873279aaccd7340a
Hamza Feroze <hamzaferoze115@gmail.com>
2024-11-11 13:03:53 +0000

    Created the webpage

---
+++ main.js
@@ -0,0 +1,261 @@
+var app = new Vue({
+    el: '#app',
+    // storing the state of the page
+    data: {
+        connected: false,
+        ros: null,
+        logs: [],
+        loading: false,
+        rosbridge_address: 'wss://i-09e03dfaefc03b18b.robotigniteacademy.com/4c5d9069-c542-4faa-bb5a-45e8ff3d3e90/rosbridge/',
+        port: '9090',
+        goal: null,
+        action: {
+            goal: { position: {x: 0, y: 0, z: 0} },
+            feedback: { position: 0, state: 'idle' },
+            result: { success: false },
+            status: { status: 0, text: '' },
+        },  
+        // dragging data
+        dragging: false,
+        x: 'no',
+        y: 'no',
+        dragCircleStyle: {
+            margin: '0px',
+            top: '0px',
+            left: '0px',
+            display: 'none',
+            width: '75px',
+            height: '75px',
+        },
+        // joystick valules
+        joystick: {
+            vertical: 0,
+            horizontal: 0,
+        },
+        // 3D stuff
+        viewer: null,
+        tfClient: null,
+        urdfClient: null,
+        // map stuff
+        mapViewer: null,
+        mapGridClient: null,
+        interval: null,
+    },
+    // helper methods to connect to ROS
+    methods: {
+        connect: function() {
+            this.loading = true
+            this.ros = new ROSLIB.Ros({
+                url: this.rosbridge_address
+            })
+            this.ros.on('connection', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
+                this.connected = true
+                this.loading = false
+                this.setup3DViewer()
+                this.setCamera()
+                this.pubInterval = setInterval(this.publish, 100)
+            })
+            this.ros.on('error', (error) => {
+                this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
+            })
+            this.ros.on('close', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
+                this.connected = false
+                this.loading = false
+                this.unset3DViewer()
+                document.getElementById('map').innerHTML = ''
+                document.getElementById('divCamera').innerHTML = ''
+            })
+            this.mapViewer = new ROS2D.Viewer({
+                divID: 'map',
+                width: 650,
+                height: 360
+            })
+
+            // Setup the map client.
+            this.mapGridClient = new ROS2D.OccupancyGridClient({
+                ros: this.ros,
+                rootObject: this.mapViewer.scene,
+                continuous: true,
+            })
+            // Scale the canvas to fit to the map
+            this.mapGridClient.on('change', () => {
+                this.mapViewer.scaleToDimensions(0.15 * this.mapGridClient.currentGrid.width, 0.15 * this.mapGridClient.currentGrid.height);
+                this.mapViewer.shift(0.15 * this.mapGridClient.currentGrid.pose.position.x, 0.15 * this.mapGridClient.currentGrid.pose.position.y)
+            })
+
+
+        },
+        disconnect: function() {
+            this.ros.close()
+            this.goal = null
+        },
+        publish: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: this.joystick.vertical, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: -this.joystick.horizontal, },
+            })
+            topic.publish(message)
+        },
+        sendGoal: function() {
+            let actionClient = new ROSLIB.ActionClient({
+                ros : this.ros,
+                serverName : '/tortoisebot_as',
+                actionName : 'course_web_dev_ros/WaypointActionAction'
+            })
+
+            this.goal = new ROSLIB.Goal({
+                actionClient : actionClient,
+                goalMessage: {
+                    ...this.action.goal
+                }
+            })
+
+            this.goal.on('status', (status) => {
+                this.action.status = status
+            })
+
+            this.goal.on('feedback', (feedback) => {
+                this.action.feedback = feedback
+            })
+
+            this.goal.on('result', (result) => {
+                this.action.result = result
+            })
+
+            this.goal.send()
+        },
+        cancelGoal: function() {
+            this.goal.cancel()
+        },
+        // joystick functionality
+        sendCommand: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0.5, },
+            })
+            topic.publish(message)
+        },
+        startDrag() {
+            this.dragging = true
+            this.x = this.y = 0
+        },
+        stopDrag() {
+            this.dragging = false
+            this.x = this.y = 'no'
+            this.dragCircleStyle.display = 'none'
+            this.resetJoystickVals()
+        },
+        doDrag(event) {
+            if (this.dragging) {
+                this.x = event.offsetX
+                this.y = event.offsetY
+                let ref = document.getElementById('dragstartzone')
+                this.dragCircleStyle.display = 'inline-block'
+
+                let minTop = ref.offsetTop - parseInt(this.dragCircleStyle.height) / 2
+                let maxTop = minTop + 200
+                let top = this.y + minTop
+                this.dragCircleStyle.top = `${top}px`
+
+                let minLeft = ref.offsetLeft - parseInt(this.dragCircleStyle.width) / 2
+                let maxLeft = minLeft + 200
+                let left = this.x + minLeft
+                this.dragCircleStyle.left = `${left}px`
+
+                this.setJoystickVals()
+            }
+        },
+        setJoystickVals() {
+            this.joystick.vertical = -1 * ((this.y / 200) - 0.5)
+            this.joystick.horizontal = +1 * ((this.x / 200) - 0.5)
+        },
+        resetJoystickVals() {
+            this.joystick.vertical = 0
+            this.joystick.horizontal = 0
+        },
+        setGoal: function(x, y) {
+            this.action.goal.position.x = x;
+            this.action.goal.position.y = y;
+        },
+        // Publisher
+        pubInterval: null,
+        // 3D visualization
+        setup3DViewer() {
+            this.viewer = new ROS3D.Viewer({
+                background: '#cccccc',
+                divID: 'div3DViewer',
+                width: 650,
+                height: 360,
+                antialias: true,
+                fixedFrame: 'odom'
+            })
+
+            // Add a grid.
+            this.viewer.addObject(new ROS3D.Grid({
+                color:'#BA0021',
+                cellSize: 0.5,
+                num_cells: 20
+            }))
+
+            // Setup a client to listen to TFs.
+            this.tfClient = new ROSLIB.TFClient({
+                ros: this.ros,
+                angularThres: 0.01,
+                transThres: 0.01,
+                rate: 10.0
+            })
+
+            // Setup the URDF client.
+            this.urdfClient = new ROS3D.UrdfClient({
+                ros: this.ros,
+                param: 'robot_description',
+                tfClient: this.tfClient,
+                // We use "path: location.origin + location.pathname"
+                // instead of "path: window.location.href" to remove query params,
+                // otherwise the assets fail to load
+                path: location.origin + location.pathname,
+                rootObject: this.viewer.scene,
+                loader: ROS3D.COLLADA_LOADER_2
+            })
+        },
+        unset3DViewer() {
+            document.getElementById('div3DViewer').innerHTML = ''
+        },
+        // camera properties
+        setCamera: function() {
+            let without_wss = this.rosbridge_address.split('wss://')[1]
+            console.log(without_wss)
+            let domain = without_wss.split('/')[0] + '/' + without_wss.split('/')[1]
+            console.log(domain)
+            let host = domain + '/cameras'
+            let viewer = new MJPEGCANVAS.Viewer({
+            divID: 'divCamera',
+            host: host,
+            width: 650,
+            height: 450,
+            topic: '/camera/image_raw',
+            ssl: true,
+            })
+        },
+    },
+    mounted() {
+        this.interval = setInterval(() => {
+            if (this.ros != null && this.ros.isConnected) {
+                this.ros.getNodes((data) => { }, (error) => { })
+            }
+        }, 10000)
+        window.addEventListener('mouseup', this.stopDrag)
+    },
+})
